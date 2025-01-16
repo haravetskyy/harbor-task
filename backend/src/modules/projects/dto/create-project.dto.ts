36d94e5551e6cabdea1cee31684e2203d483b24a
haravetskyy <yuraharavetskyy@gmail.com>
2025-01-16 04:35:26 +0100

    fix(add-poject): Delete check for emojis

--- backend/src/modules/projects/dto/create-project.dto.ts
+++ backend/src/modules/projects/dto/create-project.dto.ts
@@ -15,10 +15,6 @@ export class CreateProjectDto {
   @IsNotEmpty()
   @MaxLength(1)
   @IsString()
-  @Matches(
-    /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F900}-\u{1F9FF}\u{1F1E0}-\u{1F1FF}]+/u,
-    { message: 'Emoji must be a valid emoji' },
-  )
   emoji: string;
 
   @IsNotEmpty()
