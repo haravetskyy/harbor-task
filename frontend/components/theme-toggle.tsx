2a88338399c35fd5e9d1792ed09293eb71fe0053
haravetskyy <yuraharavetskyy@gmail.com>
2025-02-25 23:29:01 +0100

    fix(theme-toggle): Optimize ThemeToggle component and fix its behaviour in Chrome and Safari

--- frontend/components/theme-toggle.tsx
+++ frontend/components/theme-toggle.tsx
@@ -1,32 +1,26 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
-import * as React from 'react';
-
-import { Button } from '@/components/ui/button';
 
 const ThemeToggle = () => {
   const { setTheme, theme } = useTheme();
-  const [icon, setIcon] = React.useState(theme);
 
   const toggleTheme = () => {
     if (theme === 'light') {
       setTheme('dark');
-      setIcon('dark');
-    }
-    if (theme === 'dark') {
+    } else {
       setTheme('light');
-      setIcon('light');
     }
   };
 
   return (
     <Button variant="outline" size="icon" onClick={toggleTheme}>
-      {icon === 'light' ? (
+      {theme === 'light' ? (
         <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       ) : (
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       )}
     </Button>
   );
