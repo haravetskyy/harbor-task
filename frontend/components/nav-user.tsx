ccf11131020d3c7cc538a6ad72f2f494b7a35475
haravetskyy <yuraharavetskyy@gmail.com>
2025-03-09 22:32:32 +0100

    feat(nav-user): Remove `Avatar` from dropdown menu in `nav-user` component

--- frontend/components/nav-user.tsx
+++ frontend/components/nav-user.tsx
@@ -64,7 +64,10 @@ export function NavUser() {
             align="end"
             sideOffset={4}>
             <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+              <div
+                className={`${
+                  isMobile && 'hidden'
+                } flex items-center gap-2 px-1 py-1.5 text-left text-sm`}>
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage src={user.image} alt={user.name} />
                   <AvatarFallback className="rounded-lg">{getInitials(user.name)}</AvatarFallback>
@@ -75,7 +78,7 @@ export function NavUser() {
                 </div>
               </div>
             </DropdownMenuLabel>
-            <DropdownMenuSeparator />
+            <DropdownMenuSeparator className={isMobile ? 'hidden' : ''} />
             <DropdownMenuGroup>
               <DropdownMenuItem>
                 <User />
