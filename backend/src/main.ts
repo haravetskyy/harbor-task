66cae48f85bdca6905b71d602e0118b3b72da809
haravetskyy <yuraharavetskyy@gmail.com>
2025-03-24 22:37:56 +0100

    feat(ngrok): Add ngrok for sharing dev url in future

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,10 +9,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const port = configService.get<number>('PORT', 3000);
-  const corsOrigin = configService.get<string>(
-    'CORS_ORIGIN',
-    'http://localhost:5173',
-  );
+  const corsOrigin = configService.get<string>('CORS_ORIGIN', 'http://localhost:5173');
 
   console.log(`Server is starting on port: ${port}`);
 
